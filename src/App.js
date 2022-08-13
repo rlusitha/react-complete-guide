b4c6da1372f0f0a5b64fa63f32c63b822692507c
rlusitha <rlusitha@gmail.com>
2022-08-13 12:41:28 +0530

    Adding normal Javascript logic to components

--- src/App.js
+++ src/App.js
@@ -29,25 +29,25 @@ function App() {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date.toString()}
+        date={expenses[0].date}
       />
 
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date.toString()}
+        date={expenses[1].date}
       />
 
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date.toString()}
+        date={expenses[2].date}
       />
 
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
-        date={expenses[3].date.toString()}
+        date={expenses[3].date}
       />
     </div>
   );
